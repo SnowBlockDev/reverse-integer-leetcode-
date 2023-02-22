0de44541fff942a3c5f85d11fb5bdca21a017e28
Deji Snow <109693282+SnowBlockDev@users.noreply.github.com>
2023-02-22 19:24:03 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -4,17 +4,16 @@ let arr =[]
 var toStr = x.toString()
 var strValue = ""
 for(var i = 0; i < toStr.length; i++){
-    arr.push(toStr[i])
-    
+//Pushing the looped strings to the array
+    arr.push(toStr[i])   
 }
 for(var i = 0; i < toStr.length; i++){ 
+//Reversing the array into a new string 
     strValue+=arr.pop()
 }
-
 var result = parseInt(strValue)
-
-
 if (x < 0){
+//Setting the -2^31 integer limit 
     if(result <= (2)**31){
         total = result * -1
     }else{
